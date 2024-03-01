244b79209e60bea61eec38146371ce909a8dd5ea
Sanket Shirode <30955166+sanketshirode@users.noreply.github.com>
2024-03-01 15:28:03 +0530

    Create run_multiple_times.sh

---
+++ run_multiple_times.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Number of times to run the Scala application
+num_runs=5
+# Continuous loop to run the Scala application
+while true; do
+    for ((i=1; i<=$num_runs; i++)); do
+        echo "Running HelloWorld for the $i time"
+        scala HelloWorld
+    done
+    sleep 4000  # Adjust sleep duration as needed
+done
